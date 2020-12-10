d8f5ac95d4ee79d261d7045911df15f36c96d0e3
Adrian <Adrian>
2020-12-10 16:12:11 +0100

    login function ready to test

--- requests/user_requests.js
+++ requests/user_requests.js
@@ -1,18 +1,75 @@
 var db = require('../database');
 
 function createUser(req, res) {
-    var sql = "INSERT INTO users VALUES (0, '" + req.name + "')";
+    var sql = "INSERT INTO users VALUES ('0','" + req.name + "','" + req.password + "','" + req.email + "')";
     db.connection.query(sql, function (err, result) {
         if (err) {
             console.log('error on something');
             //console.log(err);
-            res.status(500).send('Something broke!');
+            res.status(400).send('Something broke!');
+        } else {
+            console.log("1 record inserted");
+            res.status(200).send('Everything OK, the id is:'+ result.insertId);
         }
-        else {
+    });
+}
+
+function connectUser(req, res) {
+    var sql = "SELECT id FROM users WHERE email='" + req.email + "' && password ='" + req.password + "'";
+    db.connection.query(sql, function (err, result) {
+        if (err) {
+            console.log('error on something');
+            //console.log(err);
+            res.status(400).send('Something broke!');
+        } else {
+            console.log("1 record inserted");
+            res.status(200).send('Everything OK, the id is:'+ result.insertId);
+        }
+    });
+}
+
+function ChangeMail(req, res) {
+    var sql = "UPDATE users SET email ='" + req.email + "' WHERE id = " + req.id + "')";
+    db.connection.query(sql, function (err, result) {
+        if (err) {
+            console.log('error on something');
+            //console.log(err);
+            res.status(400).send('Something broke!');
+        } else {
             console.log("1 record inserted");
             res.status(200).send('Everything OK');
         }
     });
 }
 
-module.exports.newuser = createUser;
+function ChangeName(req, res) {
+    var sql = "UPDATE users SET name ='' WHERE id = " + req.id + ""
+    db.connection.query(sql, function (err, result) {
+        if (err) {
+            console.log('error on something');
+            //console.log(err);
+            res.status(400).send('Something broke!');
+        } else {
+            console.log("1 record inserted");
+            res.status(200).send('Everything OK');
+        }
+    });
+}
+
+function ChangePassword(req, res) {
+    var sql = "UPDATE users SET password ='' WHERE id = " + req.id + ""
+    db.connection.query(sql, function (err, result) {
+        if (err) {
+            console.log('error on something');
+            //console.log(err);
+            res.status(400).send('Something broke!');
+        } else {
+            console.log("1 record inserted");
+            res.status(200).send('Everything OK');
+        }
+    });
+}
+
+
+module.exports.createUser = createUser;
+module.exports.connectUser = connectUser;
--- routes/user.js
+++ routes/user.js
@@ -4,7 +4,7 @@ var requests = require('../requests/user_requests');
 
 // Requête GET
 router.get('/get', function (req, res) {
-  requests.newuser(req.query);
+  requests.createUser(req.query);
   res.json({name : req.query.name});
 })
 
@@ -12,7 +12,14 @@ router.get('/get', function (req, res) {
 router.post('/create', function (req, res) {
   console.log('post called');
   console.log(req.body);
-  requests.newuser(req.body, res);
+  requests.createUser(req.body, res);
+});
+
+// Requête Connection
+router.post('/login', function (req, res) {
+  console.log('post called');
+  console.log(req.body);
+  requests.connectUser(req.body, res);
 });
 
 module.exports = router;
